d744c346af7f24d7a6c893ed3726e9dd2a81db84
Gibson Paiva <gibson.paiva94@gmail.com>
2020-01-23 16:28:54 -0300

    ok

---
+++ variaveis/exemplo-02 .php
@@ -0,0 +1,16 @@
+<?php
+    $anoNascimento = 1990;
+
+    $nome1 =  "gibson";
+
+    $sobrenome = " paiva";
+
+    $nomeCompleto = $nome1 + $sobrenome;
+
+    echo $nomeCompleto;
+    
+    
+
+    echo $nome1;
+    
+    ?>
